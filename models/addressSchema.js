17b04921209fffcac370e77990cc4ddec4d67d12
Gopika Dileep <gopikadileep76@gmail.com>
2024-12-02 10:00:20 +0530

    f

---
+++ models/addressSchema.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const{Schema} = mongoose;
+
+const addressSchema= new Schema({
+    userId:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    address:[{
+         addressType:{
+            type:string,
+            required:true
+         },
+         name:{
+            type:string,
+            required:true,
+         },
+         city:{
+            type:String,
+            required :true
+         },
+         landMark:{
+            type:String,
+            required:true
+         },
+         state:{
+            type:String,
+            required:true
+         },
+         pincode:{
+            type:Number,
+            required:true
+         },
+         phone:{
+            typq:String,
+            required:true
+         },
+         altPhone:{
+            type:String,
+            required:true,
+         }
+
+    }]
+
+})
+
+
+
+const Address = mongoose.model("Address,addressSchema");
+
+module.
